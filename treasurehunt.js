d03bb5a139a821dd20df8992466befa506caac9e
AdrianRamirez7 <adrian.ramirez@45679@gmail.com>
2023-06-25 16:22:09 -0700

    Treasure Hunt

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,18 +1,32 @@
+//His code
+
+var element = document.getElementById('myElement');
+element.style.color = 'red';
+element.style.fontSize = '20px';
+
+
+
+
 const treasure = (location) => {
     const treasureLocation = Math.floor(Math.random() * 9);
     const bombLocation = Math.floor(Math.random() * 9);
   
     if (location === treasureLocation) {
-      document.getElementById(location).innerHTML = "🪙";
+      document.getElementById(location).innerHTML = "🏆";
       alert("You found the treasure! You win!");
     } else if (location === bombLocation) {
       document.getElementById(location).innerHTML = "💣";
       alert("Oh no! You clicked on a bomb! Game over!");
     } else {
-      document.getElementById(location).innerHTML = " ";
+      document.getElementById(location).innerHTML = "💀";
+        //cell.innerHTML = "🍏";
+        //cell.onclick = null;
     }
   };
 
+ 
+
   
+
 
   
